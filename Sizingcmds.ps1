11d4499f98e7449078e154f23d82ebb4d661b41b
Vivek Shinde <vivek.s.shinde@gmail.com>
2023-01-17 17:34:19 -0500

    Delete Sizingcmds.ps1

--- Sizingcmds.ps1
+++
@@ -1,7 +0,0 @@
-﻿$objs = Import-Csv -Delimiter "," sqlsrv.csv
-foreach($item in $objs) {
-$sqlsrv = $item.InstanceName +", " + $item.Port 
-sqlcmd -H $sqlsrv -i SizingCPU.sql -o output\SizingCPU.csv -s"|"
-sqlcmd -H $sqlsrv -i SizingMemory.sql -o output\SizingMemory.csv -s"|"
-sqlcmd -H $sqlsrv -i SizingIOPS.sql -o output\SizingIOPS.csv -s"|"
-}
