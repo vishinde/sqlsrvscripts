f7ddeae7de2c6225ca9ecd685dcc3c58c521a07c
Vivek Shinde <vivek.s.shinde@gmail.com>
2023-01-18 21:16:48 -0500

    Delete RunAssessment.bat

--- RunAssessment.bat
+++
@@ -1,5 +0,0 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'c:\sqlsrvscripts\sqlsrvscripts-main\InstanceReview.ps1'"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'c:\sqlsrvscripts\sqlsrvscripts-main\DBAssess.ps1'"
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'c:\sqlsrvscripts\sqlsrvscripts-main\SizingAssess.ps1'"
-
-pause
