1eb3eb2553e675f52a49d8e96873dc2dd06f88d4
Vivek Shinde <vivek.s.shinde@gmail.com>
2023-02-14 22:58:36 -0500

    update

--- sql2sqlassess/InstanceReview.ps1
+++ sql2sqlassess/InstanceReview.ps1
@@ -3,5 +3,5 @@ foreach($item in $objs) {
 $sqlsrv = $item.InstanceName +", " + $item.Port 
 sqlcmd -H $sqlsrv -i ComponentsInstalled.sql -U test -P P@ssword1 -s"|" | findstr /v /c:"---" > output\ComponentsInstalled.csv
 sqlcmd -H $sqlsrv -i ServerProperties.sql -U test -P P@ssword1 -s"|" | findstr /v /c:"---" > output\ServerProperties.csv
-sqlcmd -H $sqlsrv -i Features.sql -U test -P P@ssword1 -s"|" | findstr /v /c:"---" > output\Features.csv 
+sqlcmd -H $sqlsrv -i Features.sql -U test -P P@ssword1 -m 1 -s"|" | findstr /v /c:"---" > output\BlockingFeatures.csv 
 }
